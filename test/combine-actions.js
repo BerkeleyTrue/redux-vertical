b727aa67125b0451126a82fd8039e18325c1d47e
Berkeley Martinez <berkeleytrue@users.noreply.github.com>
2017-09-01 00:32:58 -0700

    refactor(combineActions): Format

--- test/combine-actions.js
+++ test/combine-actions.js
@@ -3,8 +3,5 @@ import test from 'ava';
 import { combineActions } from '../src';
 
 test('should return a string', t => {
-  t.is(
-    typeof combineActions('foo', 'bar'),
-    'string'
-  );
+  t.is(typeof combineActions('foo', 'bar'), 'string');
 });
