9bf272b60b8b81b7ff4a06307a88ab1f0be2cf33
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-08-08 15:09:53 -0700

    fix(combine-actions): use string to string, not object

--- src/combine-actions.ts
+++ src/combine-actions.ts
@@ -1,6 +1,6 @@
 import config from './config';
 
-const toString = Object.prototype.toString;
+const toString = String.prototype.toString;
 
 export default function combineActions(...types: string[]): string {
   return types.map((type) => toString.call(type)).join(config.separator);
--- test/combine-actions.ts
+++ test/combine-actions.ts
@@ -1,5 +1,9 @@
-import combineActions from '../src/combine-actions';
+import { config, combineActions } from '../src';
 
 test('should return a string', () => {
   expect(typeof combineActions('foo', 'bar')).toBe('string');
+});
+
+test('should combine types', () => {
+  expect(combineActions('foo', 'bar')).toBe('foo' + config.separator + 'bar');
 });
