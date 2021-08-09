a687106d76bfb4b23dddd8e7adf498a9be57247f
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-08-08 17:31:28 -0700

    feat(compose-reducers): change api and fix test
    BREAKING CHANGE: changes api
    
    new api

--- src/compose-reducers.ts
+++ src/compose-reducers.ts
@@ -3,14 +3,8 @@ import invariant from 'invariant';
 import type { Reducer, Action } from './types';
 
 export default function composeReducers<S, A extends Action, R extends Reducer>(
-  ns: string,
   ...reducers: Array<R>
 ): (arg0: S, arg1: A) => S {
-  invariant(
-    typeof ns === 'string',
-    `ns should be a string but was given ${ns} instead`,
-  );
-
   reducers.forEach((reducer) =>
     invariant(
       typeof reducer === 'function',
--- test/compose-reducers.ts
+++ test/compose-reducers.ts
@@ -1,18 +1,14 @@
+import _ from 'lodash/fp';
+
 import { composeReducers } from '../src';
 
-test('should throw if ns is not a string', () => {
-  expect(() => composeReducers()).toThrow();
-  expect(() => composeReducers('foo', (s) => s)).not.toThrow();
-});
-
 test('should return a reducer', () => {
-  expect(typeof composeReducers('foo', (s) => s)).toBe('function');
+  expect(_.isFunction(composeReducers((s) => s))).toBe(true);
   const reducer = composeReducers(
-    'app',
     (state, { type }) => (type === 'foo' ? 1 : state),
     (state, { type }) => (type === 'bar' ? 2 : state),
   );
-  const actual0 = reducer(0, {});
+  const actual0 = reducer(0, { type: 'no' });
   const actual1 = reducer(0, { type: 'foo' });
   expect(actual1).toBe(1);
   const actual2 = reducer(0, { type: 'bar' });
@@ -21,9 +17,8 @@ test('should return a reducer', () => {
 });
 
 test('should thread state through reducers', () => {
-  expect(typeof composeReducers('foo', (s) => s)).toBe('function');
+  expect(typeof composeReducers((s) => s)).toBe('function');
   const reducer = composeReducers(
-    'app',
     (state, { type }) => (type === 'foo' ? 1 : state),
     (state, { type }) => {
       expect(state).toBe(1);
@@ -35,20 +30,10 @@ test('should thread state through reducers', () => {
 
 test('should not mutate state', () => {
   const reducer = composeReducers(
-    'app',
     (state = {}, { type }) => (type === 'foo' ? { foo: 1 } : state),
     (state = {}, { type }) => (type === 'bar' ? { bar: 1 } : state),
   );
   const original = { foo: 2, bar: 2 };
   expect(original).not.toBe(reducer(original, { type: 'bar' }));
   expect(original).toBe(reducer(original, { type: 'hello' }));
-});
-
-test('should stringify to ns', () => {
-  const reducer = composeReducers(
-    'app',
-    (state, { type }) => (type === 'foo' ? 1 : state),
-    (state, { type }) => (type === 'bar' ? 2 : state),
-  );
-  expect(String(reducer)).toBe('app');
 });
